52b5ddc0f2345deedc6d299e3e7116c960a802eb
acer7877 <46747235+acer7877@users.noreply.github.com>
2021-06-17 11:46:41 -0600

    New valve script

---
+++ UnityProject/BoilerCommissioning/Assets/Scripts/ObjectScripts/BValve.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+using VRTK.Controllables;
+using VRTK.Controllables.ArtificialBased;
+using static VRTK.Controllables.VRTK_BaseControllable;
+
+public class BValve : BGameObject
+{
+    public GameObject Hinge;
+    VRTK_ArtificialRotator rotator;
+    bool m_isOn;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        VRTKIO.isGrabbable = false;
+
+        if (Hinge == null)
+            Hinge = GameObject.Find("Hinge");
+
+        rotator = BAddComponent<VRTK_ArtificialRotator>(gameObject);
+        rotator.operateAxis = OperatingAxis.yAxis;
+        rotator.angleLimits.minimum = 0;
+        rotator.angleLimits.maximum = 90;
+        rotator.hingePoint = Hinge.transform;
+        rotator.minMaxThresholdAngle = 5;
+
+        m_isOn = false;
+    }
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        rotator.MaxLimitReached += valveOnByPlayer;
+        rotator.MinLimitReached += valveOffByPlayer;
+    }
+
+    protected override void OnDisable()
+    {
+        base.OnDisable();
+        rotator.MaxLimitReached -= valveOnByPlayer;
+        rotator.MinLimitReached -= valveOffByPlayer;
+    }
+
+
+    void valveOnByPlayer(object sender, ControllableEventArgs e)
+    {
+        if (StageManager.instance.CurrentStage == StageManager.EnumStage.Operatie)
+        {
+            m_isOn = true;
+            ObjectManager.instance.Action(name, BChecker.eCheckAction.ECA_Valve_on);
+        }
+    }
+
+    void valveOffByPlayer(object sender, ControllableEventArgs e)
+    {
+        if (StageManager.instance.CurrentStage == StageManager.EnumStage.Operatie)
+        {
+            m_isOn = false;
+            ObjectManager.instance.Action(name, BChecker.eCheckAction.ECA_Valve_off);
+        }
+    }
+}
